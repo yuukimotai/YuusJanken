a79d813faf7ccbadb07729e04bbf68eab55ebeaa
yuukimotai <yuus.saddharma.training@gmail.com>
2022-12-07 18:42:42 +0900

    Initial Commit

---
+++ YuusJanken/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  YuusJanken
+//
+//  Created by 茂田井雄輝 on 2022/11/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var answerNumber: Int = 0
+    
+    var body: some View {
+        
+        if answerNumber == 0 {
+            
+            Text("ジャンケンする?")
+                .padding(.bottom)
+        } else if answerNumber == 1 {
+            
+            Image("gu")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Spacer()
+                .frame(height: 256)
+            
+            Text("グー")
+                .padding(.bottom)
+        } else if answerNumber == 2 {
+            
+            Image("choki")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Spacer()
+                .frame(height: 128)
+            
+            Text("チョキ")
+                .padding(.bottom)
+        } else {
+            
+            Image("pa")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Spacer()
+                .frame(height: 128)
+            
+            Text("パー")
+                .padding(.bottom)
+        }
+        
+        VStack{
+
+            Button(action: {
+                
+                var newAnswerNumber = 0
+                
+                repeat {
+                    
+                    newAnswerNumber = Int.random(in: 1...3)
+                } while answerNumber == newAnswerNumber
+                
+                answerNumber = newAnswerNumber
+            }) {
+                
+                Text("ジャンケンする")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .font(.title)
+                    .background(Color.pink)
+                    .foregroundColor(Color.white)
+            }
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YuusJanken/YuusJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  YuusJankenApp.swift
+//  YuusJanken
+//
+//  Created by 茂田井雄輝 on 2022/11/09.
+//
+
+import SwiftUI
+
+@main
+struct YuusJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
